4c7d8e1f435a615c5bf20f1e63edaf7e9282c648
tehhowch <tehhowchmh@gmail.com>
2018-02-09 12:13:29 -0600

    Better wrapper function for SQL Get requests

--- mhcc/maintenance/mhcc_maintainer/mhcc_maintainer.py
+++ mhcc/maintenance/mhcc_maintainer/mhcc_maintainer.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import csv
 import random
+import json
 
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpError
@@ -23,7 +24,9 @@ FusionTables = None;
 # Script which performs maintenance functions for the MHCC FusionTable and
 # initiates a resumable upload to handle the large dataset
 def Initialize():
-	'''Read in the FusionTable ID and the api keys for oAuth''';
+	'''
+	Read in the FusionTable ID and the api keys for oAuth
+	'''
 	global localKeys;
 	with open('auth.txt') as f:
 		for line in f:
@@ -46,7 +49,9 @@ def Initialize():
 
 
 def Authorize():
-	'''Authenticate the requested Google API scopes for a single user.'''
+	'''
+	Authenticate the requested Google API scopes for a single user.
+	'''
 	flow = OAuth2WebServerFlow(localKeys['client_id'], localKeys['client_secret'], scope);
 	storage = Storage('credentials.dat');
 	credentials = storage.get();
@@ -58,38 +63,67 @@ def Authorize():
 	global FusionTables;
 	print('Authorizing...', end='');
 	FusionTables = build('fusiontables', 'v2', credentials=credentials);
+	if FusionTables is None:
+		raise EnvironmentError('FusionTables not authenticated or built as a service.');
 	print('Authorized.');
 	
 
 
-def GetQueryResult(query):
-	'''Perform a FusionTable query and return the result as a list.
-	If the response is larger than 10 MB, this will use a resumable / media download request.
-	The query is assumed to be complete, i.e. specifying the target table.'''
-	if FusionTables is None:
-		raise EnvironmentError('FusionTables not authenticated or built as a service.');
-		return '';
+def GetQueryResult(query, rowSize = 1., offsetStart = 0, maxReturned = float("inf")):
+	'''
+	Perform a FusionTable query and return the fusiontables#sqlresponse object.
+	If the response would be larger than 10 MB, this will perform several requests.
+	The query is assumed to be complete, i.e. specifying the target table, and also
+	appendable (i.e. has no existing LIMIT or OFFSET parameters).
+	'''
 	if not ValidateQuery(query):
-		return '';
-	result = {};
-	response = FusionTables.query().sqlGet(sql=query);
-	try:
-		result = response.execute();
-	except HttpError as e:
-		# Try as a media download instead.
-		
 		return {};
+	
+	# Multi-query parameters.
+	limitValue = int(9.5 * 1024 / rowSize);
+	offsetValue = offsetStart;
+	# Eventual return value.
+	ft = {'kind': "fusiontables#sqlresponse"};
+	data = [];
+	done = False;
+	while not done:
+		tail = ' '.join(['OFFSET', offsetValue.__str__(), 'LIMIT', limitValue.__str__()]);
+		request = FusionTables.query().sqlGet(sql = query + ' ' + tail);
+		try:
+			response = request.execute(num_retries = 2);
+		except HttpError as e:
+			r = json.loads(request.to_json());
+			print('Body length:', len(''.join([r['uri'], '?', r['body']])));
+			print('Start of body:', r['body'][0:200]);
+			print(e);
+			return {};
+		except Exception as e:
+			print(e);
+			return {};
+		else:
+			offsetValue += limitValue;
+			if 'rows' in response.keys():
+				data.extend(response['rows']);
+			if 'rows' not in response.keys() or len(response['rows']) < limitValue or len(data) >= maxReturned:
+				done = True;
+			if 'columns' not in ft.keys() and 'columns' in response.keys():
+				ft['columns'] = response['columns'];
 
-	if not ValidateQueryResult(result):
-		return {};
-	return ExtractQueryResult(result);
+	# Ensure that the requested maximum return count is obeyed.
+	while len(data) > maxReturned:
+		data.pop();
+	
+	ft['rows'] = data;
+	return ft;
 
 
 
 def ValidateQuery(query):
-	'''Inspect the given query to ensure it is actually a query and includes a table.'''
+	'''
+	Inspect the given query to ensure it is actually a query and includes a table.
+	'''
 	l = query.lower();
-	if 'select' not in l:
+	if ('select' not in l) and ('show' not in l) and ('describe' not in l):
 		return False;
 	if 'from' not in l:
 		return False;
@@ -98,30 +132,77 @@ def ValidateQuery(query):
 
 
 def ValidateQueryResult(queryResult):
-	'''Checks the returned response from FusionTables to ensure it has output.'''
-	if (not queryResult) or ('rows' not in queryResult.keys()) or (not queryResult['rows']):
+	'''
+	Checks the returned query result from FusionTables to ensure it has the minimum value.
+	bytestring: a newline separator (i.e. header and a value).
+	dictionary: 'rows' object (i.e. fusiontables#sqlresponse)
+	'''
+	if not queryResult:
+		return false;
+	# A dictionary query result should have a 'rows' key.
+	if type(queryResult) == dict and ('rows' not in queryResult.keys()):
+		return False;
+	# A bytestring query result should have at least one header and one value.
+	if type(queryResult) == type(b'') and len(queryResult.splitlines()) == 0:
 		return False;
 	return True;
 
 
 
-def ExtractQueryResult(queryResult):
-	'''Extracts the given response from FusionTables into an array-like result. Specifically, gets the 'rows' property.'''
-	if not queryResult or not queryResult['rows']:
-		return [];
-	return queryResult;
+def ExtractQueryResultFromByteString(queryResult):
+	'''
+	Convert the received bytestring from an alt=media request into a true FusionTables JSON response.
+	'''
+	if not queryResult:
+		return {};
+	ft = {'kind': "fusiontables#sqlresponse"};
+	columnSeparator = ',';
+	s = queryResult.decode();
+	data = [];
+	for i in s.splitlines():
+		data.append(i.split(columnSeparator));
+	ft['columns'] = data.pop(0);
+	ft['rows'] = data;
+	return ft;
 
 
 
-def GetUserBatch(start, limit):
-	'''Return up to @limit members, beginning with the index number @start
-	Uses SQLGet since only GET is done.''';
-	sql = 'SELECT Member, UID FROM ' + tableList['users'] + ' ORDER BY Member ASC OFFSET ' + start.__str__() + ' LIMIT ' + limit.__str__();
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+	Refs https://stackoverflow.com/a/34325723
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+
+def GetUserBatch(start, limit = 10000):
+	'''
+	Get a set of members and their internal IDs.
+	@params:
+		start		- Required	: the first index to return a user for (unsigned).
+		limit		- Optional	: the number of data pairs to be returned (unsigned).
+	'''
+	sql = 'SELECT Member, UID FROM ' + tableList['users'] + ' ORDER BY Member ASC';
 	print('Fetching at most', limit, 'members, starting with', start);
-	start = time.perf_counter();
-	resp = GetQueryResult(sql);
+	startTime = time.perf_counter();
+	resp = GetQueryResult(query = sql, offsetStart = start, maxReturned = limit);
 	try:
-		print('Fetched', len(resp['rows']), 'members in', round(time.perf_counter() - start, 1), ' sec.');
+		print('Fetched', len(resp['rows']), 'members in', round(time.perf_counter() - startTime, 1), ' sec.');
 		return resp['rows'];
 	except:
 		print('Received no data from user fetch query.')
@@ -129,24 +210,32 @@ def GetUserBatch(start, limit):
 
 
 def GetTotalRowCount(tableID):
-	'''Queries the size of a table, in rows.
-	Uses SQLGet since only GET is done.''';
+	'''
+	Queries the size of a table, in rows.
+	@params:	tableID	- Required	: the FusionTable to determine a row count for.
+	@return:	long
+	'''
 	countSQL = 'select COUNT(ROWID) from ' + tableID;
 	print('Fetching total row count for table id', tableID);
 	start = time.perf_counter();
 	allRowIds = GetQueryResult(countSQL);
 	try:
-		print('Fetched total row count in', round(time.perf_counter() - start, 1),'sec.');
-		return allRowIds['rows'][0];
+		print('Found', int(allRowIds['rows'][0][0]), 'rows in', round(time.perf_counter() - start, 1), 'sec.)');
+		return int(allRowIds['rows'][0][0]);
 	except:
 		print('Received no data from row count query.');
-		return 0;
+		return int(0);
 
 
 def RetrieveWholeRecords(rowids, tableID):
-	'''Returns a list of lists (i.e. 2D array) corresponding to the full records
+	'''
+	Returns a list of lists (i.e. 2D array) corresponding to the full records
 	associated with the requested rowids in the specified table.
-	Uses SQLGet since only GET is done.''';
+	@params:
+		rowids		- Required	: the rows in the table to be fully obtained (unsigned)
+		tableID		- Required	: the FusionTable to obtain records from (String)
+	@return:	fusiontable#sqlresponse
+	'''
 	if type(rowids) is not list:
 		raise TypeError('Expected list of rowids.');
 	elif not rowids:
@@ -159,8 +248,9 @@ def RetrieveWholeRecords(rowids, tableID):
 	numNeeded = len(rowids);
 	rowids.reverse();
 	baseSQL = 'SELECT * FROM ' + tableID + ' WHERE ROWID IN (';
-	print('Retrieving', numNeeded, 'records.');
+	print('Retrieving', numNeeded, 'records:');
 	startTime = time.perf_counter();
+	printProgressBar(numNeeded - len(rowids), numNeeded, "Record retrieval: ", "", 1, 50);
 	while rowids:
 		tailSQL = '';
 		sqlROWIDs = [];
@@ -169,16 +259,17 @@ def RetrieveWholeRecords(rowids, tableID):
 			sqlROWIDs.append(rowids.pop());
 			tailSQL = ','.join(sqlROWIDs) + ')';
 		# Fetch the batch of records.
-		resp = GetQueryResult(''.join([baseSQL, tailSQL]));
+		resp = GetQueryResult(''.join([baseSQL, tailSQL]), 0.3);
 		try:
 			records.extend(resp['rows']);
 		except:
-			print('Failed on query: ' + ''.join([baseSQL + tailSQL]));
 			return [];
 		elapsed = time.monotonic() - batchTime;
 		# Rate Limit
 		if elapsed < .75:
 			time.sleep(.75 - elapsed);
+		printProgressBar(numNeeded - len(rowids), numNeeded, "Record retrieval: ", "", 1, 50);
+	
 	if len(records) != numNeeded:
 		raise LookupError('Obtained different number of records than specified');
 	print('Retrieved', numNeeded, 'records in', time.perf_counter() - startTime,'sec.');
@@ -187,9 +278,11 @@ def RetrieveWholeRecords(rowids, tableID):
 
 
 def IdentifyDiffSeenAndRankRecords(uids, tableID):
-	'''Returns a list of rowids for which the LastSeen values are unique, or the
+	'''
+	Returns a list of rowids for which the LastSeen values are unique, or the
 	rank is unique (for a given LastSeen), for the given members.
-	Uses SQLGet since only GET is done.''';
+	Uses SQLGet since only GET is done.
+	'''
 	rowids = [];
 	if not tableID:
 		tableID = tableList['crowns'];
@@ -200,8 +293,10 @@ def IdentifyDiffSeenAndRankRecords(uids, tableID):
 	lastSeen = {};
 	kept = set();
 	savings = 0;
+	memberCount = len(uids);
 	baseSQL = 'SELECT ROWID, Member, UID, LastSeen, Rank FROM ' + tableID + ' WHERE UID IN (';
-	print('Sifting through', len(uids), 'member\'s stored data.');
+	print('Sifting through', memberCount, 'member\'s stored data.');
+	printProgressBar(memberCount - len(uids), memberCount, "Members sifted: ", "", 1, 50);
 	while uids:
 		tailSQL = '';
 		sqlUIDs = [];
@@ -209,11 +304,10 @@ def IdentifyDiffSeenAndRankRecords(uids, tableID):
 		while uids and (len(baseSQL + tailSQL) <= 8000):
 			sqlUIDs.append(uids.pop());
 			tailSQL = ','.join(sqlUIDs) + ') ORDER BY LastSeen ASC';
-		resp = GetQueryResult(''.join([baseSQL, tailSQL]));
+		resp = GetQueryResult(''.join([baseSQL, tailSQL]), 0.2);
 		try:
 			totalRecords = len(resp['rows']);
 		except:
-			print('Failed query: ' + ''.join([baseSQL, tailSQL]));
 			return [];
 		lastSeen.clear();
 		kept.clear();
@@ -243,15 +337,18 @@ def IdentifyDiffSeenAndRankRecords(uids, tableID):
 		elapsed = time.monotonic() - batch;
 		if elapsed < .75:
 			time.sleep(.75 - elapsed);
-
+		printProgressBar(memberCount - len(uids), memberCount, "Members sifted: ", "", 1, 50);
+		
 	print('Found', savings, 'values to trim from', savings + len(rowids), 'records');
 	return rowids;
 
 
 
 def KeepInterestingRecords(tableID):
-	'''Removes duplicated crown records, keeping each member's records which
-	have a new LastSeen value, or a new Rank value.''';
+	'''
+	Removes duplicated crown records, keeping each member's records which
+	have a new LastSeen value, or a new Rank value.
+	'''
 	startTime = time.perf_counter();
 	if not tableID:
 		tableID = tableList['crowns'];
@@ -283,7 +380,9 @@ def KeepInterestingRecords(tableID):
 
 
 def BackupTable(tableID):
-	'''Creates a copy of the existing MHCC CrownRecord Database and logs the new table id.''';
+	'''
+	Creates a copy of the existing MHCC CrownRecord Database and logs the new table id.
+	'''
 	if not tableID:
 		tableID = tableList['crowns'];
 	backup = FusionTables.table().copy(tableId=tableID).execute();
@@ -299,17 +398,21 @@ def BackupTable(tableID):
 
 
 def MakeMediaFile(values, path):
-	'''Returns a MediaFile with UTF-8 encoding, for use with FusionTable API calls
+	'''
+	Returns a MediaFile with UTF-8 encoding, for use with FusionTable API calls
 	that expect a media_body parameter.
-	Also creates a hard disk backup (to facilitate the MediaFile creation).''';
+	Also creates a hard disk backup (to facilitate the MediaFile creation).
+	'''
 	MakeLocalCopy(values, path, 'w');
 	return MediaFileUpload(path, mimetype='application/octet-stream');
 
 
 
 def MakeLocalCopy(values, path, fileMode):
-	'''Writes the given values to disk in the given location.
-	Example path: 'staging.csv' -> write file 'staging.csv' in the script's directory.'''
+	'''
+	Writes the given values to disk in the given location.
+	Example path: 'staging.csv' -> write file 'staging.csv' in the script's directory.
+	'''
 	if (not values) or (not path):
 		raise ValueError('Needed both values to save and a path to save.');
 	if not fileMode:
@@ -322,8 +425,10 @@ def MakeLocalCopy(values, path, fileMode):
 
 
 def GetSizeEstimate(values, numSamples):
-	'''Estimates the upload size of a 2D array without needing to make the actual
-	upload file. Averages @numSamples different rows to improve result statistics.''';
+	'''
+	Estimates the upload size of a 2D array without needing to make the actual
+	upload file. Averages @numSamples different rows to improve result statistics.
+	'''
 	rowCount = len(values);
 	if numSamples >= .1 * rowCount:
 		numSamples = round(.03 * rowCount);
