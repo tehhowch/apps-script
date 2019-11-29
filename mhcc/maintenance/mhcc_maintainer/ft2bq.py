33a0ecf7bf329579e905447cf728cd73b5d51421
tehhowch <tehhowch@users.noreply.github.com>
2019-11-29 15:19:54 -0600

    more work on export script
    need to type coerce values in rows, due to load job errors

--- mhcc/maintenance/mhcc_maintainer/ft2bq.py
+++ mhcc/maintenance/mhcc_maintainer/ft2bq.py
@@ -1,7 +1,9 @@
 """Script for exporting all FusionTables to a corresponding BigQuery Table"""
+import csv
 from datetime import datetime
 from re import sub as regex_replace
 from services import FusionTableHandler
+from regression_fixer import get_as_int
 
 from google.cloud import bigquery
 
@@ -14,15 +16,14 @@ def create_dataset(client: bigquery.Client, dataset_name: str, description: str 
     ds.description = description
     return client.create_dataset(ds)
 
-def decode_fusionTable_schema(ft: FusionTableHandler) -> dict:
-    """Returns a dictionary mapping from a FusionTable ID to its corresponding BigQuery TableColumn Schema"""
-    all_tables = []
-    request = ft.table.list(fields="items(name,tableId,description,columns(name,columnId,description,type,formatPattern))")
-    while request is not None:
-        response = request.execute()
-        all_tables.extend(response.get('items', []))
-        request = ft.table.list_next(request, response)
-
+def decode_fusionTable_schema(tables: list) -> dict:
+    """Returns a dictionary mapping from a FusionTable ID to its corresponding BigQuery TableColumn Schema
+    @params
+        `tables`: list['dict']
+            A list of dicts, each of which contains the basic identity and schema metadata associated with a FusionTable
+    @returns
+        { [fusiontableId]: list[BigQuery.SchemaField] }
+    """
     def _map_col(col: dict) -> dict:
         col_schema = {k: col.get(k, '') for k in ('name', 'columnId', 'description')}
         if col['name'] != 'Comment':
@@ -39,7 +40,7 @@ def decode_fusionTable_schema(ft: FusionTableHandler) -> dict:
         table_schema['columns'] = list(map(_map_col, table['columns']))
         return table_schema
 
-    return dict((s.get('tableId'), s) for s in map(_map_table, all_tables))
+    return dict((s.get('tableId'), s) for s in map(_map_table, tables))
 
 def create_tables(client: bigquery.Client, tableSchemas: dict) -> dict:
     """Create empty BigQuery tables for the given partial Table schemas.
@@ -73,3 +74,64 @@ def create_tables(client: bigquery.Client, tableSchemas: dict) -> dict:
         ftId: client.create_table(_table_from_ft(ftSchema))
             for (ftId, ftSchema) in tableSchemas.items()
     }
+
+def upload_table_data(client: bigquery.Client, tableRef: bigquery.Table, fusionFile: str) -> bigquery.LoadJob:
+    """Given the client, BigQuery table target, and data, upload the data"""
+    with open(fusionFile, mode='rb') as file:
+        job = client.load_table_from_file(file, tableRef)
+    return job
+
+def download_table_data(ft: FusionTableHandler, tableId: str, schema: dict) -> list:
+    """Download the data from the given FusionTable and process it to match the given schema"""
+    data: dict = ft.get_query_result(f'select * from {tableId}')
+    if 'rows' in data:
+        transform_table_data(data['rows'], schema)
+        return data['rows']
+
+def transform_table_data(tableRows: list, colSchema: list):
+    """Convert floats to ints where required prior to uploading. Convert NaN to 0 for numeric types"""
+    return
+    for row in tableRows:
+        # if int, use get_as_int and if result == None, set = 0
+    return
+
+def write_table_data(tableId: str, tableRows: list):
+    """Write the given data to local disk in prep for uploading"""
+    filename = f'table_{tableId}.csv'
+    with open(filename, 'w', newline='', encoding='utf-8') as f_:
+        csv.writer(f_, quoting=csv.QUOTE_NONNUMERIC).writerows(tableRows)
+    return filename
+
+def export(ft: FusionTableHandler, client: bigquery.Client, allTables=True, tableIds: list = None):
+    """Exports either all known FusionTables, or the given FusionTable IDs, to BigQuery"""
+    schemas = dict()
+    if allTables:
+        all_tables = []
+        request = ft.table.list(fields="items(name,tableId,description,columns(name,columnId,description,type,formatPattern))")
+        while request is not None:
+            response = request.execute()
+            all_tables.extend(response.get('items', []))
+            request = ft.table.list_next(request, response)
+
+        schemas.update(decode_fusionTable_schema(all_tables))
+    elif not tableIds:
+        return
+    else:
+        raise NotImplementedError()
+
+    jobs = []
+    for (tableId, tableRef) in create_tables(client, schemas).items():
+        rows = download_table_data(ft, tableId, schemas.get(tableId))
+        job: bigquery.LoadJob = upload_table_data(client, tableRef, write_table_data(tableId, rows))
+        job.add_done_callback(lambda tableId, job: print(f'Load job {"finished" if not job.error_result() else "failed"} for {tableId}'))
+        jobs.append(job)
+
+    while True:
+        if all(job.running() == False for job in jobs):
+            print('Done exporting')
+            break
+        elif any(job.error_result for job in jobs):
+            for job in jobs:
+                if job.running():
+                    job.cancel()
+
